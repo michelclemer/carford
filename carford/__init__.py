eea13a63a4a48c3cc6a399ea4f8d0137fe7da10a
michelclemer <michelclemermonteirodeolino@gmail.com>
2022-04-01 09:33:21 -0300

    config: organizando diretórios

---
+++ carford/__init__.py
@@ -0,0 +1,8 @@
+# Import flask and template operators
+from flask import Flask, render_template
+
+# Import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Define the WSGI application object
+app = Flask(__name__)
